ef3985d5e4dcf543ebcaf7bc6833152c7990740b
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-09-22 01:00:03 +0530

    AppBar V-1.0

--- Frontend/src/components/AppBar.jsx
+++ Frontend/src/components/AppBar.jsx
@@ -3,17 +3,14 @@ import { ArrowUp } from 'lucide-react';
 
 const AppBar = () => {
   return (
-    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white rounded-lg shadow-lg">
-      <div className="flex items-center p-4">
-        <button className="hover:bg-gray-700">
-          <ArrowUp size={20} />
-        </button>
+    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-slate-500 text-white rounded-lg shadow-lg">
+      <div className="flex items-center p-2">
         <div className="border-l border-gray-600 h-8"></div>
-        <button className="px-4 py-2 hover:bg-gray-700">Home</button>
-        <button className="px-4 py-2 hover:bg-gray-700">Education</button>
-        <button className="px-4 py-2 hover:bg-gray-700">Skills</button>
-        <button className="px-4 py-2 hover:bg-gray-700">Projects</button>
-        <button className="px-4 py-2 hover:bg-gray-700">Contact Me</button>
+        <button className="px-4 py-2 m-1 bg-gray-700 rounded-lg border hover:bg-gray-900 ">Home</button>
+        <button className="px-4 py-2 m-1 bg-gray-700 rounded-lg border hover:bg-gray-900 ">Education</button>
+        <button className="px-4 py-2 m-1 bg-gray-700 rounded-lg border hover:bg-gray-900 ">Skills</button>
+        <button className="px-4 py-2 m-1 bg-gray-700 rounded-lg border hover:bg-gray-900 ">Projects</button>
+        <button className="px-4 py-2 m-1 bg-gray-700 rounded-lg border hover:bg-gray-900 ">Contact Me</button>
         <div className="border-l border-gray-600 h-8"></div>
       </div>
     </div>
