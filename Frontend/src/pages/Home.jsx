eaf7d5ee7c154434498131d19ff63a8254b176be
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-09-23 02:31:42 +0530

    Create division for relevent sections and Add font for tailwind config

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,11 +5,26 @@ import AppBar from '../components/AppBar';
 
 export default function Home() {
   return (
-    <div>
+    <div className="font-roboto">
       <Header />
+      <div id='home' className='bg-red-500 h-screen'>
+        <h1 className="text-4xl font-bold">Lakshitha</h1>
+      </div>
+      <div id='education' className=' bg-red-300 h-screen'>
+
+      </div>
+      <div id='skills' className=' bg-red-300 h-screen'>
+
+      </div>
+      <div id='projects' className=' bg-red-100 h-screen'>
+
+      </div>
+      <div id='contact' className=' bg-red-00 h-screen'>
+
+      </div>
       {/* Your other app content */}
       <AppBar />
-      <Footer />    
+      <Footer />
     </div>
   )
 }
