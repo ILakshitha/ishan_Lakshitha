dd01a468c76c66fbbc8d27db09766b0e40123b5c
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-09-23 23:43:36 +0530

    create refs for landing page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import AppBar from '../components/AppBar';
 
 export default function Home() {
+  const homeref = useRef(null);
+  const educationref = useRef(null);
+  const skillref = useRef(null);
+  const projectsref = useRef(null);
+  const contactref = useRef(null);
+
+  
   return (
     <div className="font-roboto">
       <Header />
-      <div id='home' className='bg-red-500 h-screen'>
+      <div ref={homeref} id='home' className='bg-red-500 h-screen'>
         <h1 className="text-4xl font-bold">Lakshitha</h1>
       </div>
-      <div id='education' className=' bg-red-300 h-screen'>
+      <div ref={educationref} id='education' className=' bg-red-300 h-screen'>
 
       </div>
-      <div id='skills' className=' bg-red-300 h-screen'>
+      <div ref={skillref} id='skills' className=' bg-red-300 h-screen'>
 
       </div>
-      <div id='projects' className=' bg-red-100 h-screen'>
+      <div ref={projectsref} id='projects' className=' bg-red-100 h-screen'>
 
       </div>
-      <div id='contact' className=' bg-red-00 h-screen'>
+      <div ref={contactref} id='contact' className=' bg-red-00 h-screen'>
 
       </div>
       {/* Your other app content */}
